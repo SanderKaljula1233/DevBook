b999fe4065a0ec07aa1b4b4368f00e0b5f058d6b
sanderkaljula <sanderkaljula4@gmail.com>
2019-01-13 22:33:48 +0200

    redux bug

--- client/src/store.js
+++ client/src/store.js
@@ -1,24 +1,20 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import rootReducer from './reducers';
-
-const initialState = {};
+import reducer from './reducers';
 
 const middleware = [thunk];
 
-const whichCompose = () => {
-    if (window.navigator.userAgent.includes("Chrome")) {
-      compose(
-        applyMiddleware(...middleware),
-        // Implements the Chrome redux tools extension
-        window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      );
-    } else {
-      compose(applyMiddleware(...middleware));
-    }
-  };
-   
-  const store = createStore(reducers, {}, whichCompose());
+const composeEnhancers =
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?   
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+    }) : compose;
+
+const enhancer = composeEnhancers(
+  applyMiddleware(...middleware),
+  // other store enhancers if any
+);
+const store = createStore(reducer, enhancer);
 
 export default store;
